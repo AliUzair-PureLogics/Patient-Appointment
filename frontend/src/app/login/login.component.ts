ae998ee2c03e5c7b1d6c5c277301d60ec18746bd
Ali Uzair <ali.uzair@purelogics.net>
2019-01-04 16:11:56 +0500

    Made simple login form

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @Input() person;
   constructor() { }
 
   ngOnInit() {
+    this.person = {
+      username: '',
+      password: ''
+    };
+  }
+
+  doLogin() {
+    if (!this.person.username) {
+      return alert(`Please enter username`);
+    }
+    if (!this.person.password) {
+      return alert(`Please enter password`);
+    }
+    alert(`Got request to login with username='${this.person.username}' and password='${this.person.password}'`);
   }
 
 }
